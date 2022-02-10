9de1c0afb60d59488a541d4d671a8dd80d8399ce
Phu1237 <phu1237@gmail.com>
2022-02-10 10:12:13 +0700

    update npm packages version

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -2,7 +2,6 @@
 
 namespace Phu1237\TailwindAuth\Console;
 
-use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
 
@@ -37,54 +36,54 @@ class InstallCommand extends Command
         }
 
         // Controllers...
-        (new Filesystem)->ensureDirectoryExists(app_path('Http/Controllers/Auth'));
-        (new Filesystem)->copyDirectory(__DIR__ . '/../../stubs/App/Http/Controllers/Auth', app_path('Http/Controllers/Auth'));
+        (new Filesystem())->ensureDirectoryExists(app_path('Http/Controllers/Auth'));
+        (new Filesystem())->copyDirectory(__DIR__.'/../../stubs/App/Http/Controllers/Auth', app_path('Http/Controllers/Auth'));
 
         // Requests...
-        (new Filesystem)->ensureDirectoryExists(app_path('Http/Requests/Auth'));
-        (new Filesystem)->copyDirectory(__DIR__ . '/../../stubs/App/Http/Requests/Auth', app_path('Http/Requests/Auth'));
+        (new Filesystem())->ensureDirectoryExists(app_path('Http/Requests/Auth'));
+        (new Filesystem())->copyDirectory(__DIR__.'/../../stubs/App/Http/Requests/Auth', app_path('Http/Requests/Auth'));
 
         // Base install
         if ($this->option('core')) {// Routes...
             $this->appendToFile('require __DIR__.\'/auth.php\';', base_path('routes/web.php'));
-            copy(__DIR__ . '/../../stubs/routes/auth.php', base_path('routes/auth.php'));
+            copy(__DIR__.'/../../stubs/routes/auth.php', base_path('routes/auth.php'));
             // Replace the HOME path to '/'
             $this->replaceInFile('/home', '/', app_path('Providers/RouteServiceProvider.php'));
         }
 
         // Just export if no option --controllers
         if (!$this->option('controllers') && !$this->option('core')) {// Views...
-            (new Filesystem)->ensureDirectoryExists(resource_path('views/auth'));
-            (new Filesystem)->ensureDirectoryExists(resource_path('views/layouts'));
+            (new Filesystem())->ensureDirectoryExists(resource_path('views/auth'));
+            (new Filesystem())->ensureDirectoryExists(resource_path('views/layouts'));
             // Empty blade
             if ($this->option('empty')) {
-                (new Filesystem)->copyDirectory(__DIR__ . '/../../stubs/resources/views_empty/auth', resource_path('views/auth'));
-                (new Filesystem)->copyDirectory(__DIR__ . '/../../stubs/resources/views_empty/layouts', resource_path('views/layouts'));
+                (new Filesystem())->copyDirectory(__DIR__.'/../../stubs/resources/views_empty/auth', resource_path('views/auth'));
+                (new Filesystem())->copyDirectory(__DIR__.'/../../stubs/resources/views_empty/layouts', resource_path('views/layouts'));
             } else {
-                (new Filesystem)->copyDirectory(__DIR__ . '/../../stubs/resources/views/auth', resource_path('views/auth'));
-                (new Filesystem)->copyDirectory(__DIR__ . '/../../stubs/resources/views/layouts', resource_path('views/layouts'));
+                (new Filesystem())->copyDirectory(__DIR__.'/../../stubs/resources/views/auth', resource_path('views/auth'));
+                (new Filesystem())->copyDirectory(__DIR__.'/../../stubs/resources/views/layouts', resource_path('views/layouts'));
                 // Tailwind / Webpack...
                 // NPM Packages...
                 $this->updateNodePackages(function ($packages) {
                     return [
-                            '@tailwindcss/forms' => '^0.2.1',
-                            'autoprefixer' => '^10.1.0',
-                            'postcss' => '^8.2.1',
-                            'postcss-import' => '^12.0.1',
-                            'tailwindcss' => '^2.0.2',
-                        ] + $packages;
+                        '@tailwindcss/forms' => '^0.4.0',
+                        'autoprefixer' => '^10.1.0',
+                        'postcss' => '^8.2.1',
+                        'postcss-import' => '^14.0.1',
+                        'tailwindcss' => '^3.0.0',
+                    ] + $packages;
                 });
-                copy(__DIR__ . '/../../stubs/tailwind.config.js', base_path('tailwind.config.js'));
-                copy(__DIR__ . '/../../stubs/webpack.mix.js', base_path('webpack.mix.js'));
-                copy(__DIR__ . '/../../stubs/resources/css/app.css', resource_path('css/app.css'));
+                copy(__DIR__.'/../../stubs/tailwind.config.js', base_path('tailwind.config.js'));
+                copy(__DIR__.'/../../stubs/webpack.mix.js', base_path('webpack.mix.js'));
+                copy(__DIR__.'/../../stubs/resources/css/app.css', resource_path('css/app.css'));
             }
 
             // Tests...
-            (new Filesystem)->copyDirectory(__DIR__ . '/../../stubs/tests/Feature', base_path('tests/Feature'));
+            (new Filesystem())->copyDirectory(__DIR__.'/../../stubs/tests/Feature', base_path('tests/Feature'));
 
             // Routes...
             $this->appendToFile('require __DIR__.\'/auth.php\';', base_path('routes/web.php'));
-            copy(__DIR__ . '/../../stubs/routes/auth.php', base_path('routes/auth.php'));
+            copy(__DIR__.'/../../stubs/routes/auth.php', base_path('routes/auth.php'));
 
             // Replace the HOME path to '/'
             $this->replaceInFile('/home', '/', app_path('Providers/RouteServiceProvider.php'));
@@ -96,7 +95,7 @@ class InstallCommand extends Command
 
     protected function backupFilesAndDirectories()
     {
-        (new Filesystem)->ensureDirectoryExists(base_path('backups'));
+        (new Filesystem())->ensureDirectoryExists(base_path('backups'));
         $array = [
             app_path('Http/Controllers/Auth'),
             app_path('Http/Requests/Auth'),
@@ -113,14 +112,14 @@ class InstallCommand extends Command
         foreach ($array as $item) {
             $explode = explode('\\', $item);
             $last = $explode[count($explode) - 1];
-            $backup_path = 'backups/' . $last;
+            $backup_path = 'backups/'.$last;
             if (file_exists($item)) {
                 if (is_file($item)) {
                     $this->ensureDirectoryOfFileExists($backup_path);
                     copy($item, base_path($backup_path));
                 } else {
                     if (is_dir($item)) {
-                        (new Filesystem)->copyDirectory($item, $backup_path);
+                        (new Filesystem())->copyDirectory($item, $backup_path);
                     }
                 }
             }
@@ -131,9 +130,10 @@ class InstallCommand extends Command
     }
 
     /**
-     * Create directory for file if not existed yet
+     * Create directory for file if not existed yet.
      *
      * @param string $path
+     *
      * @return void
      */
     private static function ensureDirectoryOfFileExists($path)
@@ -142,14 +142,14 @@ class InstallCommand extends Command
         array_pop($explode);
         $dir = implode('/', $explode);
 
-        (new Filesystem)->ensureDirectoryExists(base_path($dir));
+        (new Filesystem())->ensureDirectoryExists(base_path($dir));
     }
 
     /**
      * Update the "package.json" file.
      *
-     * @param callable $callback
      * @param bool $dev
+     *
      * @return void
      */
     protected static function updateNodePackages(callable $callback, $dev = true)
@@ -170,7 +170,7 @@ class InstallCommand extends Command
 
         file_put_contents(
             base_path('package.json'),
-            json_encode($packages, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) . PHP_EOL
+            json_encode($packages, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT).PHP_EOL
         );
     }
 
@@ -180,6 +180,7 @@ class InstallCommand extends Command
      * @param string $search
      * @param string $replace
      * @param string $path
+     *
      * @return void
      */
     protected function replaceInFile($search, $replace, $path)
@@ -188,15 +189,16 @@ class InstallCommand extends Command
     }
 
     /**
-     * Append content to the end of file
+     * Append content to the end of file.
      *
      * @param string $content
      * @param string $path
+     *
      * @return void
      */
     protected function appendToFile($content, $path)
     {
         // And line before and after the content
-        file_put_contents($path, file_get_contents($path) . "\n" . $content . "\n");
+        file_put_contents($path, file_get_contents($path)."\n".$content."\n");
     }
 }
